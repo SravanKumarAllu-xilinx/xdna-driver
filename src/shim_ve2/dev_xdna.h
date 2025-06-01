38c82cf8d3d3155c95cb932bb15a4b5a259dcb3b
SravanKumarAllu-xilinx <sravallu@amd.com>
2025-06-01 10:37:09 +0530

    XDNA migration userppace xdna-driver

---
+++ src/shim_ve2/dev_xdna.h
@@ -0,0 +1,20 @@
+#ifndef DEV_XDNA_H
+#define DEV_XDNA_H
+
+#include "core/edge/user/dev.h"
+
+namespace shim_xdna_edge {
+
+class dev_xdna : public xrt_core::edge::dev
+{
+public:
+    std::shared_ptr<xrt_core::device>
+    create_device(xrt_core::device::handle_type handle, xrt_core::device::id_type id) const override;
+
+    virtual xrt_core::device::handle_type
+    create_shim(xrt_core::device::id_type id) const override;
+};
+
+}
+
+#endif
